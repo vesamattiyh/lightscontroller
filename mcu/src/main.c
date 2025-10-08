e8385ce2eb429e0ce7a4cba4a3a82f9aacc0d392
Vesa-Matti <vmylih@gmail.com>
2025-10-08 21:28:15 +0300

    Setup the container and update the app to hello world

--- mcu/src/main.c
+++ mcu/src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_log.h"
 
 void app_main() 
 {
     const char *TAG = "example";
-    ESP_LOGI(TAG, "Hello, from ESP32!\n");
+
+    for(;;){    
+        ESP_LOGI(TAG, "Hello, from ESP32!\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    };
 }
