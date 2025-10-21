aa687002ab46c7c3c0fde42f16e1a5975e1fc024
Vesa-Matti <vmylih@gmail.com>
2025-10-21 21:41:52 +0300

    Reorganize gat services table

--- mcu/lib/bluetooth/src/gatt_svc.c
+++ mcu/lib/bluetooth/src/gatt_svc.c
@@ -12,25 +12,26 @@ static const ble_uuid128_t relay1_chr_uuid =
     BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
                      0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF1);
 
-static const ble_uuid128_t relay2_chr_uuid =
-    BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
-                     0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF2);
+// static const ble_uuid128_t relay2_chr_uuid =
+//     BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
+//                      0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF2);
 
-static const ble_uuid128_t relay3_chr_uuid =
-    BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
-                     0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF3);
+// static const ble_uuid128_t relay3_chr_uuid =
+//     BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
+//                      0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF3);
 
-static const ble_uuid128_t relay4_chr_uuid =
-    BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
-                     0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF4);
+// static const ble_uuid128_t relay4_chr_uuid =
+//     BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
+//                      0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF4);
 
-static const ble_uuid128_t relay5_chr_uuid =
-    BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
-                     0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF5);
+// static const ble_uuid128_t relay5_chr_uuid =
+//     BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
+//                      0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF5);
 
-static const ble_uuid128_t relay6_chr_uuid =
-    BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
-                     0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF6);
+// static const ble_uuid128_t relay6_chr_uuid =
+//     BLE_UUID128_INIT(0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78,
+//                      0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF6);
+struct ble_gatt_svc_def gatt_svr_svcs[2];
 
 static uint16_t relay_val_handle[6]; // Handles for each relay characteristic
 
@@ -67,81 +68,113 @@ static int relay_descriptor_access(uint16_t conn_handle, uint16_t attr_handle,
     return BLE_ATT_ERR_UNLIKELY;
 }
 
-struct ble_gatt_svc_def gatt_svr_svcs[] = {
-    /* Automation IO service */
-    {
-        .type = BLE_GATT_SVC_TYPE_PRIMARY,
-        .uuid = &auto_io_svc_uuid.u,
-        .characteristics =
-            (struct ble_gatt_chr_def[]) {
-            { .uuid = &relay1_chr_uuid.u,
-              .access_cb = relay_access,
-              .flags = BLE_GATT_CHR_F_WRITE,
-              .val_handle = &relay_val_handle[0],
-              .arg = (void *)1,
-              .descriptors = (struct ble_gatt_dsc_def[]) {
-                USER_READ_DESCRIPTOR("Relay 1", relay_descriptor_access),
+void gatt_init_services(void){
+    // struct ble_gatt_svc_def gatt_svr_svcs[2];
+
+    struct ble_gatt_svc_def auto_io;
+    auto_io.type = BLE_GATT_SVC_TYPE_PRIMARY;
+    auto_io.uuid = &auto_io_svc_uuid.u;
+
+    struct ble_gatt_chr_def relay_chr_defs[MAX_RELAYS+1];
+
+    for(int i=0; i<MAX_RELAYS; i++){
+        relay_chr_defs[i] = 
+        (struct ble_gatt_chr_def){
+            .uuid = &relay1_chr_uuid.u,
+            .access_cb = relay_access,
+            .flags = BLE_GATT_CHR_F_WRITE,
+            .val_handle = &relay_val_handle[i],
+            .arg = (void *)(i+1),
+            .descriptors = (struct ble_gatt_dsc_def[]) {
+                USER_READ_DESCRIPTOR(relay_get_name(i), relay_descriptor_access),
                 {0}} // Terminator
-            },
-            { .uuid = &relay2_chr_uuid.u,
-              .access_cb = relay_access,
-              .flags = BLE_GATT_CHR_F_WRITE,
-              .val_handle = &relay_val_handle[1],
-              .arg = (void *)2,
-              .descriptors = (struct ble_gatt_dsc_def[]) {
-                USER_READ_DESCRIPTOR("Relay 2", relay_descriptor_access), 
-                {0}} // Terminator
-            },
-            { .uuid = &relay3_chr_uuid.u,
-              .access_cb = relay_access,
-              .flags = BLE_GATT_CHR_F_WRITE,
-              .val_handle = &relay_val_handle[2],
-              .arg = (void *)3, 
-              .descriptors = (struct ble_gatt_dsc_def[]) {
-                USER_READ_DESCRIPTOR("Relay 3", relay_descriptor_access), 
-                {0}} // Terminator
-            },
-            { .uuid = &relay4_chr_uuid.u,
-              .access_cb = relay_access,
-              .flags = BLE_GATT_CHR_F_WRITE,
-              .val_handle = &relay_val_handle[3],
-              .arg = (void *)4, 
-              .descriptors = (struct ble_gatt_dsc_def[]) {
-                USER_READ_DESCRIPTOR("Relay 4", relay_descriptor_access), 
-                {0}} // Terminator
-            },
-            { .uuid = &relay5_chr_uuid.u,
-              .access_cb = relay_access,
-              .flags = BLE_GATT_CHR_F_WRITE,
-              .val_handle = &relay_val_handle[4],
-              .arg = (void *)5, 
-              .descriptors = (struct ble_gatt_dsc_def[]) {
-                USER_READ_DESCRIPTOR("Relay 5", relay_descriptor_access), 
-                {0}} // Terminator
-            },
-            { .uuid = &relay6_chr_uuid.u,
-              .access_cb = relay_access,
-              .flags = BLE_GATT_CHR_F_WRITE,
-              .val_handle = &relay_val_handle[5],
-              .arg = (void *)6,
-              .descriptors = (struct ble_gatt_dsc_def[]) {
-                USER_READ_DESCRIPTOR("Relay 6", relay_descriptor_access), 
-                {0}} // Terminator
-            },
-            { 0 } // terminator
-            }, // End of characteristics array
-    }, // End of service
-    
-    {
-        0, /* Termination, No more services. */
-    },
+        };
+    }
+    relay_chr_defs[MAX_RELAYS] = (struct ble_gatt_chr_def){0}; // Terminator for defs
+
+    auto_io.characteristics = relay_chr_defs;
+
+    gatt_svr_svcs[0] = auto_io;
+    gatt_svr_svcs[1] = (struct ble_gatt_svc_def) {0};
 };
+
+// struct ble_gatt_svc_def gatt_svr_svcs[] = {
+//     /* Automation IO service */
+//     {
+//         .type = BLE_GATT_SVC_TYPE_PRIMARY,
+//         .uuid = &auto_io_svc_uuid.u,
+//         .characteristics =
+//             (struct ble_gatt_chr_def[]) {
+//             { .uuid = &relay1_chr_uuid.u,
+//               .access_cb = relay_access,
+//               .flags = BLE_GATT_CHR_F_WRITE,
+//               .val_handle = &relay_val_handle[0],
+//               .arg = (void *)1,
+//               .descriptors = (struct ble_gatt_dsc_def[]) {
+//                 USER_READ_DESCRIPTOR("Relay 1", relay_descriptor_access),
+//                 {0}} // Terminator
+//             },
+//             { .uuid = &relay2_chr_uuid.u,
+//               .access_cb = relay_access,
+//               .flags = BLE_GATT_CHR_F_WRITE,
+//               .val_handle = &relay_val_handle[1],
+//               .arg = (void *)2,
+//               .descriptors = (struct ble_gatt_dsc_def[]) {
+//                 USER_READ_DESCRIPTOR("Relay 2", relay_descriptor_access), 
+//                 {0}} // Terminator
+//             },
+//             { .uuid = &relay3_chr_uuid.u,
+//               .access_cb = relay_access,
+//               .flags = BLE_GATT_CHR_F_WRITE,
+//               .val_handle = &relay_val_handle[2],
+//               .arg = (void *)3, 
+//               .descriptors = (struct ble_gatt_dsc_def[]) {
+//                 USER_READ_DESCRIPTOR("Relay 3", relay_descriptor_access), 
+//                 {0}} // Terminator
+//             },
+//             { .uuid = &relay4_chr_uuid.u,
+//               .access_cb = relay_access,
+//               .flags = BLE_GATT_CHR_F_WRITE,
+//               .val_handle = &relay_val_handle[3],
+//               .arg = (void *)4, 
+//               .descriptors = (struct ble_gatt_dsc_def[]) {
+//                 USER_READ_DESCRIPTOR("Relay 4", relay_descriptor_access), 
+//                 {0}} // Terminator
+//             },
+//             { .uuid = &relay5_chr_uuid.u,
+//               .access_cb = relay_access,
+//               .flags = BLE_GATT_CHR_F_WRITE,
+//               .val_handle = &relay_val_handle[4],
+//               .arg = (void *)5, 
+//               .descriptors = (struct ble_gatt_dsc_def[]) {
+//                 USER_READ_DESCRIPTOR("Relay 5", relay_descriptor_access), 
+//                 {0}} // Terminator
+//             },
+//             { .uuid = &relay6_chr_uuid.u,
+//               .access_cb = relay_access,
+//               .flags = BLE_GATT_CHR_F_WRITE,
+//               .val_handle = &relay_val_handle[5],
+//               .arg = (void *)6,
+//               .descriptors = (struct ble_gatt_dsc_def[]) {
+//                 USER_READ_DESCRIPTOR("Relay 6", relay_descriptor_access), 
+//                 {0}} // Terminator
+//             },
+//             { 0 } // terminator
+//             }, // End of characteristics array
+//     }, // End of service
+    
+//     {
+//         0, /* Termination, No more services. */
+//     },
+// };
 
 int gatt_svc_init(void){
     int rc = 0;
-
+    
     /* GATT service initialization */
     ble_svc_gatt_init();
+
+    gatt_init_services();
 
     /* Update GATT services counter */
     rc = ble_gatts_count_cfg(gatt_svr_svcs);
